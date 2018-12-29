140e26f4101e7b57fe69bdb656a863b8f8e44444
Weihao Qu <weihaoqu@Weihaos-MacBook-Pro.local>
2018-12-28 19:39:22 -0500

    modify tworounds

--- adapt ocaml/tworound.ml
+++ adapt ocaml/tworound.ml
@@ -6,7 +6,7 @@ open Printf
 type 'a bang  = 
     Bang of 'a 
 type database = int list list
-type query = database -> int
+type query = database -> float
 type mech = query -> database -> float
 type result = float * int
 
@@ -18,23 +18,32 @@ let sum_q (db: int list list) =
     List.fold_left ( fun a r -> a +  (hd r)  ) 0 db  
 
 let gauss_mech (q:query) db =  
-   float_of_int (q db) +. (sample_normal_boxmuller3 0.0 1.0 )
+    (q db) +. (sample_normal_boxmuller3 0.0 1.0 )
 
-let rec dot (l1:int list) (l2: int list) : int =
-  match l1 , l2 with 
-  | x :: tl1, y ::tl2 -> x*y + (dot tl1 tl2)
-  | _ ,_ -> 0
+let normal_mech (q:query) db =  
+    (q db) 
+
+let dot (l1:int ) (l2: int) : int = l1 * l2
+  
 
 let sign (y: float) : int = 
   if y > 0.0 then 1 else -1
 
-let get (d:database) (i:int) : int list =
-  nth d i
+let get (row:int list) (i:int) : int  =
+  nth row i
 
 let g (m:mech) (k:int) (d1:database) : result list = 
   let rec f (m:mech) (j:int) (k:int) (d1:database) =
      if j < k then
-       let a = m (fun d -> dot (get d j) (get d (k-1) ) ) (d1) in
+       let a = m 
+       (fun d ->
+          let sm =  List.fold_left 
+          ( fun sum row ->  
+              sum + dot (get row j) (get row (k-1) )
+          ) 0 d  in  
+            (float_of_int sm) /.  float_of_int (List.length d)
+           )
+        d1 in
            (a, j):: (f m (j+1) k d1)
      else []
    in
@@ -44,19 +53,31 @@ let g (m:mech) (k:int) (d1:database) : result list =
 let two_round d k m = 
  let l = g m k d in 
   let q = fun d -> 
+   let sm =  List.fold_left ( fun sum row -> 
      sign ( List.fold_left  
+            (fun a r ->
+              let x = 1.0+.(fst r) /. (1.0 -. fst r ) in 
+                let y = float_of_int  ( get row (snd r) ) +. log x in 
+                   a +. y  
+            ) 0.0 l
+          )
+   )
+      0 d in
+      (float_of_int sm) /.  float_of_int (List.length d)
+     (* sign ( List.fold_left  
             (fun a r ->
               let x = 1.0+.(fst r) /. (1.0 -. fst r ) in 
                 let y = float_of_int  ( List.hd (get d (snd r) )) +. log x in 
                    a +. y  
             ) 0.0 l
-          )
+          ) *)
    in 
   (m q d)
 
   let main  = 
       let x = two_round dataset 4 gauss_mech in
-        printf "%f" x
+      let y =  two_round dataset 4 normal_mech in
+        printf "gauss_mech:%f, no noise mech is: %f" x y
        
 
 
