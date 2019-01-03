328592dfbd4f500707d602c684c4a65b2a0f725b
JiawenUB <jiawenliu18@gmail.com>
2019-01-02 22:31:47 -0500

    update multiround and thresholdout

--- adapt ocaml/multiround.ml
+++ adapt ocaml/multiround.ml
@@ -39,50 +39,6 @@ let parseArgs () =
 
 let dataset = [ [1;1;1;1] ; [1;1;1;1] ; [1;1;1;1] ; [1;1;1;1] ] 
 
-let rec creat_db (col : int) (row : int)  =
-  if row > 0 
-    then 
-      let rec creat_row (col : int) = 
-        if col > 0
-        then 
-          (Random.int 2) :: creat_row (col - 1)
-        else
-          []
-      in (creat_row col) :: creat_db col (row - 1)
-    else
-      []
-
-let thresholdout_mech (q:query) db =
-  let holdout_db = creat_db (List.length db) (List.length db) in
-    let threshold = 1.0 in
-      let noise_rate = 1.0 in
-        let budget = (List.length db) in
-              let rec threshold_mech db holdout_db gamma =
-                match (db, holdout_db) with
-                  |([],[]) -> []
-                  |(rw::t, holdout_rw::holdout_t) -> let eta = (sample_exp (4.0 *. noise_rate)) in
-                  if ((q rw) -. (q holdout_rw)) > (threshold +. gamma +. eta)
-                  then
-                    ((q holdout_rw) +. (sample_exp noise_rate))::(threshold_mech t holdout_t (sample_exp 2.0 *. noise_rate))
-                  else
-                    (q rw)::(threshold_mech t holdout_t)
-                  in
-                    let sm = List.fold_left 
-                      (fun sum rw ->  
-                          sum +. rw
-                      ) 0.0 (threshold_mech db holdout_db (sample_exp (2.0 *. noise_rate)))
-                    in 
-                      (sm /.  float_of_int (List.length db))
-
-
-let rec read_rows ic i j acc =
-        if i < j then
-         read_line ic (i+1) j (int_of_string (input_line ic)) :: acc
-        else
-         List.rev acc 
-       
-let read_db ic acc =
-   let rec read_rows 
 
 let sum_q (db: int list list) = 
     List.fold_left ( fun a r -> a +  (hd r)  ) 0 db  
