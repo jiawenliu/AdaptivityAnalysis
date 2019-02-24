8d41512ecdf783837e9d605f5a83570f49509d87
JiawenUB <jiawenliu18@gmail.com>
2019-02-25 04:26:18 +0800

    update the big step evaluation function

--- interpreter/ast.ml
+++ interpreter/ast.ml
@@ -2,31 +2,42 @@
 type expr =
   | Var 	of string
   | App 	of expr * expr
+  | Fix		of function * string * expr(* unsure *) 
   | Pair 	of expr * expr
-  | True
-  | False
-  | Nil
-  | Cons 	of expr*expr
-  | Let 	of string * expr * expr
   | Fst 	of expr
   | Snd 	of expr
+  | True
+  | False
   | If 		of expr * expr * expr
   | Const 	of int
   | Mech 	of expr
-  |
+  | Let 	of string * expr * expr
+  | Nil
+  | Cons 	of expr*expr
 
 
  type value = 
  	| V_True
  	| V_False
- 	| V_Var		of string
- 	| V_Const 	of int
+  	| V_Const 	of int
+  	| V_Fix		of expr * env (* unsure *) 
+  	| V_Pair 	of value * value
  	| V_Nil
+ 	| V_Cons 	of value * value
 
 
 type trace = 
+	| T_Var		of expr
+	| T_Eval	of trace * trace * expr * trace
+	| T_Fix 	of expr
+	| T_Pair	of trace * trace
+	| T_Fst		of trace
+	| T_Snd		of trace
 	| T_True
 	| T_False
-	| T_Var of string
-	| T_Const of int
+	| T_Iftrue	of trace * trace
+	| T_Iffalse	of trace * trace
+	| T_Const 	of int
+	| T_Mech 	of trace
 	| T_Nil
+	| T_Cons 	of trace * trace
--- interpreter/main.ml
+++ interpreter/main.ml
@@ -9,10 +9,33 @@ let rec subst e1 e2 x =
 (* Big step of evaluation. *)
 let rec bigstep env expr = 
   match expr with
-  | Var x               -> (V_Var (eval env x) , T_Var x)
+  | Var x               -> (eval env x , T_Var x)
   | Const c             -> (V_Const c, T_Const c)
   | True                -> (V_True, T_True)
   | False               -> (V_False, T_False)
+  | Fix f x e           -> ((V_Fix (Fix f x e) env), T_Fix (Fix f x e))
+  | App e1 e2           -> 
+    (
+      match ((bigstep env e1), (bigstep env e2)) with
+      | ((v1, t1), (v2, t2))    ->          
+        (
+          match v1 with
+          | (Fix f x e, env1)   ->
+            (
+              match (bigstep ((f, v1) :: (x, v2) :: (env1)) e) with
+              | (v, t)          -> (v, T_Eval t1 t2 (f, x) t)
+              | _               -> Error
+              )
+          | _                   -> Error
+          )
+      | _                       -> Error
+      )
+  | Pair e1 e2          -> 
+  | Fst e               ->
+  | Snd e               ->
+  | If True e1 e2       ->
+  | If False e1 e2      ->
+  | Mech e              ->
   | Nil                 -> (V_Nil, T_Nil)
 
 
