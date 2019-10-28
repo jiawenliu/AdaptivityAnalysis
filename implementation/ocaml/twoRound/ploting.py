ceb43683573ec06c8054ee083729f1e4b2190969
Jiawen <jliu223@buffalo.edu>
2019-10-28 18:05:04 -0400

    plot

--- implementation/ocaml/twoRound/ploting.py
+++ implementation/ocaml/twoRound/ploting.py
@@ -6,19 +6,23 @@ def read_lines(ifile):
 	return  [float(line) for line in open(ifile)]
 
 def read_files(dir):
-	return [(read_line(file),file) for file in glob.glob(dir)]
+	return [(read_lines(file),file) for file in glob.glob(dir)]
 
 def read_lables(dir):
 	return glob.glob(dir)
 
 def plot_lines(ys, x):
-	for  y,label in enumerate(ys):
-		plt.plot(y, x, label=label)
+	for y,label in (ys):
+		print y
+		plt.plot(x, y, label=label)
+	plt.grid(True)
+	plt.legend(loc="best")
 	plt.show()
 
-datas = read_files("results/*.txt")
+datas = read_files("results1/*.txt")
+print datas
 plot_lines(datas,  np.arange(10, 100, 5) )
 
-for  y,label in enumerate(datas):
-	plt.plot(y, np.arange(10, 100, 5), label=label)
-plt.show()
+# for  y,label in enumerate(datas):
+# 	plt.plot(y, np.arange(10, 100, 5), label=label)
+# plt.show()
