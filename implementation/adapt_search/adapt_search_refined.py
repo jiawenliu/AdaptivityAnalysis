cb4d284bdd1531af6d63c945f0f575606e5d1f0d
jiawenliu <jiawenliu18@gmail.com>
2022-05-23 21:08:20 -0400

    update testing examples into test suits, update the refined adaptivity search algorithm

--- implementation/adapt_search/adapt_search_refined.py
+++ implementation/adapt_search/adapt_search_refined.py
@@ -9,31 +9,11 @@ class AdaptSearchAlgRefined(AdaptSearchAlg):
         def __init__(self, to = -1, next = -1):
             self.to = to 
             self.next = next        
-    # vertex_no = 100000
 
-    # edges = list[edge]
-
-    # head = [-1]*10000
-    # scc_cnt = 0
-    # scc_no = [-1]*100000
-    # dfs_clock = 0
-    # first_visit = [-1]*10000
-    # last_visit = [-1]*10000
-    # adapt = [0]*100000
     def __init__(self, graph = Graph()):
-        self.graph = graph
-        self.vertex_no =graph.get_vertice_num()
-        self.edges = []
-        self.first_visit = [0]*self.vertex_no
-        self.last_visit = [0]*self.vertex_no
-        
-        self.scc_adapt = [AdaptType(0)]* (self.vertex_no + 1)
-        self.head = [-1]*self.vertex_no
-        self.scc_stack = []
-        self.scc_graph = [[]]*(self.vertex_no+1)
-        self.scc_no = [0]*self.vertex_no
-        self.scc_cnt = 0
-        self.dfs_clock = 0
+        AdaptSearchAlg.__init__(self, graph)
+        self.flow_capacity = [AdaptType(0)] * (self.vertex_no + 1)
+        self.query_num = [0] * (self.vertex_no + 1)
 
     def create_edges (self):
         for e in self.graph.edges:
@@ -71,82 +51,52 @@ class AdaptSearchAlgRefined(AdaptSearchAlg):
                 x = self.scc_stack.pop()
                 # print(x)
                 self.scc_no[x] = self.scc_cnt
-                self.scc_adapt[self.scc_cnt] = (self.scc_adapt[self.scc_cnt] + self.graph.weights[x]) if self.graph.query[x] else self.scc_adapt[self.scc_cnt]
+                # self.scc_adapt[self.scc_cnt] = (self.scc_adapt[self.scc_cnt] + self.graph.weights[x]) if self.graph.query[x] else self.scc_adapt[self.scc_cnt]
                 # print(self.scc_adapt)
                 if x == u:
                     break
             # print("new scc # : ", self.scc_cnt, "with vetices: ", self.scc_stack)
             # print(self.scc_no)
 
-    def find_scc(self):
-        self.scc_no = [0]*self.vertex_no
-        self.first_visit = [0]*self.vertex_no
-        self.scc_adapt = [AdaptType(0)]*(self.vertex_no + 1)
-        self.scc_cnt = self.dfs_clock = 0
-        for i in range(0, self.vertex_no):
-            if not self.first_visit[i]:
-                self.two_direction_dfs(i)
-        # print(self.scc_cnt, self.scc_adapt)
 
-
-    def build_scc(self):
-        self.scc_graph = [[] for _ in range(self.vertex_no+1)]
-        for u in range(0, self.vertex_no):
-            print("vertex ", u, " is the head of edge # ", self.head[u])
-            i = self.head[u]
-            print("vertex ", u, "belongs to the scc # ", self.scc_no[u])
-            while i != -1:
-                v = self.edges[i].to
-                if not self.scc_no[u] == self.scc_no[v]:
-                    self.scc_graph[self.scc_no[u]].append(self.scc_no[v])
-                i = self.edges[i].next
-        # print(self.scc_graph)
 
     def refined_adapt_calculation_dfs (self, u: int):
-        self.dfs_clock += 1
-        self.first_visit[u] = self.last_visit[u] = self.dfs_clock
-        self.scc_stack.append(u)
         i = self.head[u]
         while i != -1:
             v = self.edges[i].to
-            if not self.first_visit[v]:
-                self.two_direction_dfs(v)
-                self.last_visit[u] = min(self.last_visit[u], self.last_visit[v])
+            self.flow_capacity[v] = self.flow_capacity[u].adapt_min(self.graph.weights[v])
+            self.query_num[v] = self.query_num[u] + self.graph.query[v]
+            if not self.visited[v]:
+                self.visited[v] = True
+                self.refined_adapt_calculation_dfs(v)
                 # print("in the visiting #: ", self.dfs_clock, ". after visit vertex ", v, "the first visit is: ", self.first_visit[v], 
                 #     "the last visit is: ", self.last_visit[v])
-            elif not self.scc_no[v]:
-                self.last_visit[u] = min(self.last_visit[u], self.first_visit[v])
+            else:
+                self.adapt[v] = self.adapt[v].adapt_max(self.flow_capacity[v] * self.query_num[v])
+                self.flow_capacity[v] = self.graph.weights[v]
+                self.query_num[v] = self.graph.query[v]
                 # print("in the visiting #: ", self.dfs_clock, "the visited vertex ", v, ", whoes first visit is: ", self.first_visit[v], 
                 #     "the last visit is: ", self.last_visit[v])
             i = self.edges[i].next
         
-        if self.first_visit[u] == self.last_visit[u]:
-            self.scc_cnt += 1
-            # print("new scc # : ", self.scc_cnt, "with vetices: ", self.scc_stack)
-            while True:
-                x = self.scc_stack.pop()
-                # print(x)
-                self.scc_no[x] = self.scc_cnt
-                self.scc_adapt[self.scc_cnt] = (self.scc_adapt[self.scc_cnt] + self.graph.weights[x]) if self.graph.query[x] else self.scc_adapt[self.scc_cnt]
-                # print(self.scc_adapt)
-                if x == u:
-                    break
+
 
     def bfs_adapt(self):
         bfs_q = []
-        visited = [False]*(self.vertex_no + 1)
+        self.visited = [False]*(self.vertex_no + 1)
         self.adapt = [AdaptType(0)]* (self.scc_cnt+2)
-        for i in range(1, self.scc_cnt+1):
-            self.adapt[i] = self.scc_adapt[i] if self.scc_no[0] == i  else AdaptType(0)
+        # for i in range(1, self.scc_cnt+1):
+        #     self.adapt[i] = self.scc_adapt[i] if self.scc_no[0] == i  else AdaptType(0)
         bfs_q.append(self.scc_no[0])
         while bfs_q != []:
             u = bfs_q.pop(0)
             # print(u)
-            visited[u] = False
+            # self.visited[u] = False
+            self.refined_adapt_calculation_dfs(u)
             for v in self.scc_graph[u]:
-                self.adapt[v] = (self.adapt[v].adapt_max(self.adapt[u] + self.scc_adapt[v]))
-                if not visited[v]:
-                    visited[v] = True 
+                # self.adapt[v] = (self.adapt[v].adapt_max(self.adapt[u] + self.scc_adapt[v]))
+                if not self.visited[v]:
+                    self.visited[v] = True 
                     bfs_q.append(v)
 
     def get_adapt(self):
