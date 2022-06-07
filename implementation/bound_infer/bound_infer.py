ee80ee8444906b84918d26634ad56e84abeeb75e
jiawenliu <jiawenliu18@gmail.com>
2022-06-07 16:19:29 -0400

    adding features on bound infer algorithm. Build the base Graph Class, remove hard coded parameters

--- implementation/bound_infer/bound_infer.py
+++ implementation/bound_infer/bound_infer.py
@@ -109,6 +109,91 @@ class DifferenceConstraint:
     def get_dec_value(self):
         return self.dec_value
 
+
+class DirectedGraph:
+    def __init__(self, vertices_num = 1, edges = []) -> None:
+        self.vertices_num = vertices_num
+        self.edges = edges
+        # default dictionary to store graph
+        self.graph = defaultdict(list)
+        self.time = 0
+        self.scc_ids = [-1] * (self.vertices_num)
+        self.scc_cnt = -1
+        pass
+
+    def build_edges(self):
+        for (u, v) in self.edges:
+            self.graph[u].append(v)
+
+    def is_in_scc(self, edge):
+        if self.scc_cnt == -1:
+            self.build_edges()
+            self.build_scc()
+        (u, v) = edge
+        return self.scc_ids[u] == self.scc_ids[v]
+    
+    def scc_dfs(self,u, low, disc, stackMember, st):
+
+        # Initialize discovery time and low value
+        disc[u] = self.time
+        low[u] = self.time
+        self.time += 1
+        stackMember[u] = True
+        st.append(u)
+
+        # Go through all vertices adjacent to this
+        for v in self.graph[u]:
+            
+            # If v is not visited yet, then recur for it
+            if disc[v] == -1 :
+            
+                self.scc_dfs(v, low, disc, stackMember, st)
+
+                # Check if the subtree rooted with v has a connection to
+                # one of the ancestors of u
+                # Case 1 (per above discussion on Disc and Low value)
+                low[u] = min(low[u], low[v])
+                        
+            elif stackMember[v] == True:
+
+                '''Update low value of 'u' only if 'v' is still in stack
+                (i.e. it's a back edge, not cross edge).
+                Case 2 (per above discussion on Disc and Low value) '''
+                low[u] = min(low[u], disc[v])
+
+        # head node found, pop the stack and print an SCC
+        w = -1 #To store stack extracted vertices
+        if low[u] == disc[u]:
+            self.scc_cnt += 1
+            while w != u:
+                w = st.pop()
+                # print (w, end=" ")
+                self.scc_ids[w] = self.scc_cnt
+                stackMember[w] = False
+                
+            # print()
+   
+
+    #The function to do DFS traversal.
+    # It uses recursive scc_dfs()
+    def build_scc(self):
+
+        # Mark all the vertices as not visited
+        # and Initialize parent and visited,
+        # and ap(articulation point) arrays
+        disc = [-1] * (self.vertices_num)
+        low = [-1] * (self.vertices_num)
+        stackMember = [False] * (self.vertices_num)
+        st =[]
+        
+
+        # Call the recursive helper function
+        # to find articulation points
+        # in DFS tree rooted with vertex 'i'
+        for i in range(self.vertices_num):
+            if disc[i] == -1:
+                self.scc_dfs(i, low, disc, stackMember, st)   
+
 class Graph:
     weights = [AdaptType(1), AdaptType(1)]
     query = [1, 1]
@@ -121,18 +206,24 @@ class Graph:
     def get_vertice_num(self):
         return len(self.weights)
 
-class TransitionGraph(Graph):
-    locations = [0, 1]
+#TODO: build Base Father Graph Calss
+#Inherit the Transition Graph and the Data-Flow Graph from the Command Graph Class
+class TransitionGraph(DirectedGraph):
+    # locations = [0, 1]
     ctl_edges = [(0, 1), (1, 1)]
     transitions = [(0, [DifferenceConstraint("x", None, "k", DifferenceConstraint.DCType.RESET)], 1, [0]),
     (1, [DifferenceConstraint("x", None, "1", DifferenceConstraint.DCType.DEC)], 1, [1, 2])]
 
-    def __init__(self, edges=[(0, 1), (2, 2)], weights=[AdaptType(1), AdaptType("k"), AdaptType("k")], query=[1, 0, 1]):
-        super().__init__(edges, weights, query)
+    def __init__(self, 
+    edges=[(0, 1), (1, 1)], 
+    transitions=[(0, [DifferenceConstraint("x", None, "k", DifferenceConstraint.DCType.RESET)], 1, [0]),
+    (1, [DifferenceConstraint("x", None, "1", DifferenceConstraint.DCType.DEC)], 1, [1, 2])]
+    ):
+        super().__init__(max(map(lambda x: max(x), edges)) + 1, edges)
+        self.ctl_edges = edges
+        self.transitions = transitions
+
     
-    def is_scc(self, t):
-        scc_list = [False, True]
-        return scc_list[t]
 
 
 
@@ -144,16 +235,23 @@ class LocalBound:
     
     @staticmethod
     def compute_local_bounds(transition_graph):
-        transition_local_bounds = ["0"]*len(transition_graph.transitions)
-        for transition_index in range(len(transition_graph.transitions)):
-            (_, dc_set, _, _) = transition_graph.transitions[transition_index]
-            if not transition_graph.is_scc(transition_index):
-                transition_local_bounds[transition_index] = "1"
+        transition_local_bounds = ["-1"]*len(transition_graph.transitions)
+        for index, (_, dc_set, _, _) in enumerate((transition_graph.transitions)):
+            # (_, dc_set, _, _) = transition_graph.transitions[transition_index]
+            if not transition_graph.is_in_scc(transition_graph.edges[index]):
+                transition_local_bounds[index] = "1"
             else:
                 for dc in dc_set:
                     if dc.is_dec():
-                        transition_local_bounds[transition_index] = dc.get_var()
-        print(transition_local_bounds)
+                        transition_local_bounds[index] = dc.get_var()                    
+        
+        for index, local_bound in enumerate(transition_local_bounds):
+            # (_, dc_set, _, _) = transition_graph.transitions[transition_index]
+            if local_bound == "-1":
+                for i_other, lb_other in enumerate(transition_local_bounds):
+                    if lb_other != "-1" and (not DirectedGraph(transition_graph.vertices_num, transition_graph.edges[:i_other]+transition_graph.edges[i_other+1:]).is_in_scc(transition_graph.edges[index])):
+                        transition_local_bounds[index] = lb_other
+                        continue
         return transition_local_bounds
 
 
@@ -196,7 +294,7 @@ class TransitionBound:
     def compute_var_invariant(self, v):
         var_inc = "0"
         var_reset = "0"
-        print(v, self.var_resets[v], self.var_incs[v])
+        # print(v, self.var_resets[v], self.var_incs[v])
         for (t, dc_const) in self.var_incs[v]:
             var_inc += " + " + self.transition_bounds[t] + " * " + dc_const
         for (t, dc_var, dc_const) in self.var_resets[v]:
@@ -233,19 +331,22 @@ class TransitionBound:
         self.compute_var_inc_and_reset()
         for transition_index in range(len(self.transition_graph.transitions)):
             self.compute_transition_bound_closure(transition_index)
+        return self.transition_bounds
 
 
 class VariableReachingBound:
-    def __init__(self, graph, transition_graph) -> None:
+    def __init__(self, graph=Graph(), transition_graph=TransitionGraph()) -> None:
         self.graph = graph
         self.transition_graph = transition_graph
     
     def attach_weights(self):
-        transition_bounds = TransitionBound.compute_transition_bounds()
-        for (t, b) in transition_bounds.items():
-            for var_vertex in t[4]:
-                self.graph.weights[var_vertex] = AdaptType(b)
 
-transitionbound_algorithm = TransitionBound()
-transitionbound_algorithm.compute_transition_bounds()
-print(transitionbound_algorithm.transition_bounds)
+        transition_bounds = TransitionBound(self.transition_graph).compute_transition_bounds()
+        for (t_index, b) in enumerate(transition_bounds):
+            for var_vertex in self.transition_graph.transitions[t_index][3]:
+                self.graph.weights[var_vertex] = AdaptType(b)
+    
+    def get_weights(self):
+        return [w.value for w in self.graph.weights]
+
+
