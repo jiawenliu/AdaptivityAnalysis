67a73e7a5e3af8be5d9a734800dcbe9842fa664c
jiawenliu <jiawenliu18@gmail.com>
2023-02-23 23:37:23 -0500

    update the comparison evaluation and the reimplementation of the fitting function

--- eval/benchmark/tensorflow/mechanism/mechanized_sequential.py
+++ eval/benchmark/tensorflow/mechanism/mechanized_sequential.py
@@ -15,6 +15,7 @@ class MechanizedSequential(tf.keras.Sequential):
     self.mechanism = None
     self.mu = 0.0
     self.sigma = 0.03
+    
      
   def choose_mech(self, mech = None):
      self.mechanism = mech
@@ -50,3 +51,55 @@ class MechanizedSequential(tf.keras.Sequential):
          self.compiled_metrics.update_state(y, y_pred, sample_weight)
          return super(MechanizedSequential, self).compute_metrics(
         x, y, y_pred, sample_weight)
+      
+  def train_step(self, data):
+      # Unpack the data. Its structure depends on your model and
+      # on what you pass to `fit()`.
+      x, y = data
+      # noise = tf.random.normal(
+      #   x.shape,
+      #   mean=self.mu,
+      #   stddev=self.sigma,
+      #   dtype=tf.dtypes.float32,
+      #   seed=None,
+      #   name=None
+      #   )
+      # print("In Gaussian Mechanizem, the noised data {}".format(noise))
+      with tf.GradientTape() as tape:
+         print("In Gaussian Mechanizem")
+
+         x_noise = tf.random.normal(
+               tf.shape(x),
+               mean=self.mu,
+               stddev=self.sigma,
+               dtype=x.dtype,
+               seed=None,
+               name=None
+               )
+         noised_x = x_noise + x
+         y_pred = self(noised_x, training=True)  # Forward pass
+         # Compute the loss value
+         # (the loss function is configured in `compile()`)
+         noise = tf.random.normal(
+               tf.shape(y_pred),
+               mean=self.mu,
+               stddev=self.sigma,
+               dtype = y_pred.dtype,
+               seed=None,
+               name=None
+               )
+         
+         loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+      # Compute gradients
+      trainable_vars = self.trainable_variables
+      gradients = tape.gradient(loss, trainable_vars)
+
+      # Update weights
+      self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+      # Update metrics (includes the metric that tracks the loss)
+      self.compiled_metrics.update_state(y, y_pred)
+
+      # Return a dict mapping metric names to current value
+      return {m.name: m.result() for m in self.metrics}
