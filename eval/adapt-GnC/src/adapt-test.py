e23ffab9262de43af55e5cd21d9796ac794fe766
jiawenliu <jiawenliu18@gmail.com>
2023-02-24 16:21:40 -0500

    update the threshold and data splitting mecahnism w.r.t the programs in the tensorflow benchmarl

--- eval/adapt-GnC/src/adapt-test.py
+++ eval/adapt-GnC/src/adapt-test.py
@@ -19,10 +19,10 @@ class Runner():
 	Compatible with the Quandaric-Strategy and the mechanisms having fixed
 	dataset size n, desired significance beta (coverage should be at least 1-beta), and tolerance width tau.
 	""" 
-
+ 
 	def __init__(self):
-	    self.q_cnt = 0
-
+		self.q_cnt = 0
+	
 	def one_run_one_mech(self, n, q_max = 1000, q_adapt = 1000, mechanism = mech.Mechanism()):
 		strategy = stg.Strategy(n,  ada_freq = {"method": "additive", "method_param": q_adapt}, q_max=q_max)
 		mechanism.reset()
@@ -30,11 +30,11 @@ class Runner():
 
 		q = strategy.next_query()
 		while q:
-		    r = mechanism.get_answer(q["query"])
-		    if r[0]["answer"] is not None:
-		        q = strategy.next_query(r)
-		    else:
-		        q = None
+			r = mechanism.get_answer(q["query"])
+			if r[0]["answer"] is not None:
+				q = strategy.next_query(r)
+			else:
+				q = None
 		q_done = len(strategy.mech_ans_list)
 		print("ANS OF ADAPTIVE QUERYS:", strategy.mech_ans_list)
 		print("TRUE ANSWER OF ADAPTIVE QUERYS:", strategy.true_ans_list)
@@ -43,18 +43,18 @@ class Runner():
 
 	def runs_one_mech(self, n_list = range(500, 1001, 100), q_max_list = range(500, 1001, 100), q_adapt_list = range(500, 1001, 100), runs = 1, mechanism = mech.Mechanism()):
 
-	    rmse_list = []
-	    for n in n_list:
-	    	for i in range(len(q_max_list)):
-		    	se_matrix = [(self.one_run_one_mech(n, q_max_list[i], q_adapt_list[i], mechanism)) for _ in range(runs)]
-		        rmse_pair_list = [np.array([[l[i],1] if len(l) > i else [0, 0] for l in se_matrix]).sum(0) for i in range(max([len(l) for l in se_matrix]))]
-		        rmse = [pl[0] / pl[1] for pl in rmse_pair_list]
-		    	print("ROOT MEAN SQUARE ERROR: ", rmse)
-		        rmse_list.append(rmse)
+		rmse_list = []
+		for n in n_list:
+			for i in range(len(q_max_list)):
+				se_matrix = [(self.one_run_one_mech(n, q_max_list[i], q_adapt_list[i], mechanism)) for _ in range(runs)]
+				rmse_pair_list = [np.array([[l[i],1] if len(l) > i else [0, 0] for l in se_matrix]).sum(0) for i in range(max([len(l) for l in se_matrix]))]
+				rmse = [pl[0] / pl[1] for pl in rmse_pair_list]
+				print("ROOT MEAN SQUARE ERROR: ", rmse)
+				rmse_list.append(rmse)
 
-	    print("DATA SIZE RANGE: ", n_list)
-	    print("RMSE: ", rmse_list)
-	    return rmse_list
+		print("DATA SIZE RANGE: ", n_list)
+		print("RMSE: ", rmse_list)
+		return rmse_list
 
 	def runandplot_with_one_mech(self, q_max_list = [100], q_adapt_list = [10], mech_name = "DataSplit"):
 		runs = 500
@@ -154,8 +154,8 @@ class Runner():
 		elif mech_name == "GnC_gauss":
 		# # TODO: Tuning the parameter
 			GnC_gauss = mech.Guess_and_Check_Mechanism(mech_guess = mech.Gaussian_Mechanism(sigma=sigma),
-			    check_data_frac = check_data_frac,
-			    use_mgf_width=False)
+				check_data_frac = check_data_frac,
+				use_mgf_width=False)
 			GnC_gauss.add_params(beta = beta, tau = tau)
 			mech_rmse = self.runs_one_mech(np.array(n_list), q_max_list, q_adapt_list, runs, GnC_gauss)
 			mech_para = "GnC_gauss : SIGMA: {}, CHECK_FRAC: {}, BETA: {}, TAU: {}".format(sigma, check_data_frac, beta, tau)
@@ -172,8 +172,8 @@ class Runner():
 		## TODO: DEBUG
 		elif mech_name == "GnC_thresh":
 			GnC_thresh = mech.Guess_and_Check_Mechanism(mech_guess = mech.Thresholdout_Mechanism(hold_frac=hold_frac, threshold=threshold, sigma=sigma),
-			        check_data_frac = check_data_frac,
-			        use_mgf_width=False)
+					check_data_frac = check_data_frac,
+					use_mgf_width=False)
 			GnC_thresh.add_params(beta = beta, tau = tau)
 			mech_rmse = self.runs_one_mech(np.array(n_list), q_max_list, q_adapt_list, runs, GnC_thresh)
 			mech_para = "GnC_thresh : HOLD_FRAC: {}, T: {}, SIGMA: {}, CHECK_FRAC: {}, BETA: {}, TAU: {}".format(hold_frac, threshold, sigma, check_data_frac, beta, tau)
@@ -189,8 +189,8 @@ class Runner():
 		## TODO: DEBUG
 		elif mech_name == "GnC_DataSplit":
 			GnC_DataSplit = mech.Guess_and_Check_Mechanism(mech_guess = mech.Mechanism(),
-			        check_data_frac = check_data_frac,
-			        use_mgf_width=False)
+					check_data_frac = check_data_frac,
+					use_mgf_width=False)
 			GnC_DataSplit.add_params(beta = beta, tau = tau)
 			mech_rmse = self.runs_one_mech(np.array(n_list), q_max_list, q_adapt_list, runs, GnC_DataSplit)
 			mech_para = "GnC_DataSplit : BETA: {}, TAU: {}".format(check_data_frac, beta, tau)
@@ -208,8 +208,8 @@ class Runner():
 		## TODO: DEBUG
 		elif mech_name == "GnC_Baseline":
 			GnC_Baseline = mech.Guess_and_Check_Mechanism(mech_guess = mech.Mechanism(),
-			        check_data_frac = check_data_frac,
-			        use_mgf_width=False)
+					check_data_frac = check_data_frac,
+					use_mgf_width=False)
 			GnC_Baseline.add_params(beta = beta, tau = tau)
 			mech_rmse = self.runs_one_mech(n_list, q_max_list, q_adapt_list, runs, GnC_Baseline)
 			mech_para = "GnC_Baseline : BETA: {}, TAU: {}".format(check_data_frac, beta, tau)
@@ -230,7 +230,7 @@ class Runner():
 	def main(self):
 		q_max_list = [5000]
 		q_adapt_list = [200]
-	        
+			
 	##################################################################################################################
 	###################################### Switching Mechanisms: ######################################
 	##################################################################################################################
--- eval/benchmark/tensorflow/mechanism/mechanized_sequential.py
+++ eval/benchmark/tensorflow/mechanism/mechanized_sequential.py
@@ -1,22 +1,51 @@
 import tensorflow as tf
-
+import cw_funcs as cw
+import numpy as np
 from enum import Enum
 
 class Mechanism(Enum):
    GAUSSIAN = 1
    DATASPLIT = 2
    THRESHOLD = 3
+   PLAIN = 0
 
 class MechanizedSequential(tf.keras.Sequential):
 
 
   def __init__(self, *args, **kwargs):
-    super(MechanizedSequential, self).__init__(*args, **kwargs)
-    self.mechanism = None
-    self.mu = 0.0
-    self.sigma = 0.03
-    
-     
+      super(MechanizedSequential, self).__init__(*args, **kwargs)
+      self.mechanism = None
+
+
+      '''
+      Parameters for the GnC mechanism 
+      '''
+      self.mu = 0.0
+      self.sigma = 0.03
+
+      '''
+      Parameters for the GnC mechanism 
+      '''      
+      self.beta = None
+      self.tau = None
+      self.check_for_width = None   
+
+
+      '''
+      Parameters for the Naive Data Splitting mechanism 
+      '''
+      self.split_size = None
+
+
+      '''
+      Parameters for the Thresholdout mechanism 
+      '''
+      self.hold_size = None
+      self.train_size = None
+      self.hold_frac = None
+      self.threshold = None
+      self.noisy_thresh = None
+         
   def choose_mech(self, mech = None):
      self.mechanism = mech
   
@@ -24,19 +53,43 @@ class MechanizedSequential(tf.keras.Sequential):
      self.mu = mu
      self.sigma = sigma
 
+  def set_mechanism_para(self, mu, sigma, beta, tau, hold_frac, threshold, check_for_width=cw.bounds_Thresh):
+      self.mu = mu
+      self.sigma = sigma
+      self.beta = beta
+      self.tau = tau
+      self.check_for_width = check_for_width
+      assert 0.0 < hold_frac <= 1.0, "hold_frac should take a value in (0, 1]."
+      self.hold_frac = hold_frac
+      self.threshold = threshold
+      self.noisy_thresh = self.threshold + np.random.laplace(0, 2 * self.sigma)
+     
   def compute_metrics_gaussin(self, x, y, y_pred, sample_weight):
-      noise = tf.random.normal(
-        tf.shape(y_pred),
+      x_noise = tf.random.normal(
+            tf.shape(x),
+            mean=self.mu,
+            stddev=self.sigma,
+            dtype=x.dtype,
+            seed=None,
+            name=None
+            )
+      
+      noised_x = x_noise + x
+  
+      y_noise = tf.random.normal(
+        tf.shape(y),
         mean=self.mu,
         stddev=self.sigma,
-        dtype=tf.dtypes.float32,
+        dtype = y.dtype,
         seed=None,
         name=None
         )
-      noised_pred = y_pred + noise
-      self.compiled_metrics.update_state(y, noised_pred, sample_weight)
+      noised_y = y + y_noise
+
+      self.compiled_metrics.update_state(noised_y, y_pred, sample_weight)
+
       metrics_results = super(MechanizedSequential, self).compute_metrics(
-        x, y, noised_pred, sample_weight)
+        noised_x, y, y_pred, sample_weight)
       return metrics_results
   
   def compute_metrics(self, x, y, y_pred, sample_weight):
@@ -55,19 +108,28 @@ class MechanizedSequential(tf.keras.Sequential):
   def train_step(self, data):
       # Unpack the data. Its structure depends on your model and
       # on what you pass to `fit()`.
-      x, y = data
-      # noise = tf.random.normal(
-      #   x.shape,
-      #   mean=self.mu,
-      #   stddev=self.sigma,
-      #   dtype=tf.dtypes.float32,
-      #   seed=None,
-      #   name=None
-      #   )
-      # print("In Gaussian Mechanizem, the noised data {}".format(noise))
-      with tf.GradientTape() as tape:
-         print("In Gaussian Mechanizem")
+      if self.mechanism is None:
+        return super(MechanizedSequential, self).train_step(data)
+      elif self.mechanism == Mechanism.GAUSSIAN:
+         print("In Gaussian Mechanism")
+         return self.gaussian_train_step(data)
+      elif self.mechanism == Mechanism.DATASPLIT:
+         print("In Naive Data Splitting Mechanism")
+         return self.data_split_train_step(data)
+      elif self.mechanism == Mechanism.THRESHOLD:
+         print("In Threshold out Mechanism")
+         return self.thresholdout_train_step(data)
+      else:
+        return super(MechanizedSequential, self).train_step(data)
 
+      
+  
+  def gaussian_train_step(self, data):
+      # Unpack the data. Its structure depends on your model and
+      # on what you pass to `fit()`.
+      x, y = data
+      with tf.GradientTape() as tape:
+         print("Create noise when accessing the training data")
          x_noise = tf.random.normal(
                tf.shape(x),
                mean=self.mu,
@@ -90,6 +152,101 @@ class MechanizedSequential(tf.keras.Sequential):
                )
          
          loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+      # Compute gradients
+      trainable_vars = self.trainable_variables
+      gradients = tape.gradient(loss, trainable_vars)
+
+      # Update weights
+      self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+      # Update metrics (includes the metric that tracks the loss)
+      self.compiled_metrics.update_state(y, y_pred)
+
+      # Return a dict mapping metric names to current value
+      return {m.name: m.result() for m in self.metrics}
+  
+  '''
+  Naive Data Splitting Mechanism:
+  Either splitting the data in smaller batch-size and train with more steps,
+  or doing the same training steps as the one without any mechanism.
+  This one is the version that is the same as the train_step without any mechanism.
+  '''
+  def data_split_train_step(self, data):
+      # Unpack the data. Its structure depends on your model and
+      # on what you pass to `fit()`.
+
+      x, y = data
+      with tf.GradientTape() as tape:
+         print("In Naive Data Split")
+         y_pred = self(x, training=True)  # Forward pass
+         loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+      # Compute gradients
+      trainable_vars = self.trainable_variables
+      gradients = tape.gradient(loss, trainable_vars)
+
+      # Update weights
+      self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+      # Update metrics (includes the metric that tracks the loss)
+      self.compiled_metrics.update_state(y, y_pred)
+
+      # Return a dict mapping metric names to current value
+      return {m.name: m.result() for m in self.metrics}
+  
+  def thresholdout_train_step(self, data):
+      # Unpack the data. Its structure depends on your model and
+      # on what you pass to `fit()`.
+      x, y = data[:self.hold_frac]
+      x_hold, y_hold = data[self.hold_frac:]
+      with tf.GradientTape() as tape:
+
+         y_pred = self(x, training=True)  # Forward pass
+
+         y_pred_hold = self(x_hold, training = True)
+         
+
+         '''
+         TODO: Need to consider one of the following model as one shot of query:
+          model-1. one step of training, the result of the logistic is a query
+          model-2. one step of training, the losse of the trained logistic v.s. the true logistic.
+         '''
+
+         '''
+         model-1:
+         drawback: the query result isn't unform data type, the trained logistic has different size dependents on the databse size.
+         '''
+         #TODO: subtraction between the tensors of different size
+         if np.abs(y_pred - y_pred_hold) >= self.noisy_thresh + np.random.laplace(0, 4 * self.sigma):
+            self.noisy_thresh = self.threshold + np.random.laplace(0, 2 * self.sigma)
+            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+            #TODO: subtraction between the tensors of different size
+            y_noise = tf.random.laplace(
+                        tf.shape(y),
+                        mean=self.mu,
+                        stddev=self.sigma,
+                        dtype = y.dtype,
+                        seed=None,
+                        name=None
+                        )
+            y_pred_hold = y_pred_hold + y_noise
+
+            # y_pred = min(1.0, max(0.0, y_pred_hold + np.random.laplace(0, self.sigma)))
+            loss = self.compiled_loss(y_hold, y_pred_hold, regularization_losses=self.losses)
+
+         '''
+         model-2:
+         drawback: the "loss of  the trained logistic v.s. the true logistic" isn't a direct
+          query on the data. 
+         advantage: the losses in different steps or different size of database always have the same type.
+         '''
+         loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+         loss_hold = self.compiled_loss(y_hold, y_pred_hold, regularization_losses=self.losses)
+         if np.abs(loss - loss_hold) >= self.noisy_thresh + np.random.laplace(0, 4 * self.sigma):
+            self.cost_q += 1
+            self.noisy_thresh = self.threshold + np.random.laplace(0, 2 * self.sigma)
+            loss = min(1.0, max(0.0, loss_hold + np.random.laplace(0, self.sigma)))
 
       # Compute gradients
       trainable_vars = self.trainable_variables
