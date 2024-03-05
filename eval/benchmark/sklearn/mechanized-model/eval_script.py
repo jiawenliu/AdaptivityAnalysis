68c40585f4be204f66e1693abc088bbf1efaeccd
Weihao Qu <weihaoqu@buffalo.edu>
2024-03-05 11:58:59 -0500

    remove matplotlib

--- eval/benchmark/sklearn/mechanized-model/eval_script.py
+++ eval/benchmark/sklearn/mechanized-model/eval_script.py
@@ -6,7 +6,7 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split, KFold, GridSearchCV
